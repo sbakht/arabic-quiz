0617573bfb466f37bd02df6ab20efaa36b113ce4
Saad Bakht <rotk13@gmail.com>
2021-01-30 11:17:52 -0500

    add todo list

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,19 @@
     <div class="p-16">
       <quiz-handler></quiz-handler>
     </div>
+    <todo-list class="p-16 pt-0"></todo-list>
   </div>
 </template>
 
 <script>
 import QuizHandler from "./components/quiz.handler.vue";
+import TodoList from "./components/todo.list.vue";
 
 export default {
   name: "app",
   components: {
     QuizHandler,
+    TodoList,
   },
 };
 </script>
-
-</style>
---
+++ src/components/todo.list.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <li v-for="todo in todos" :key="todo">{{ todo }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: [
+        "Red error state with shake",
+        "correct answer green state",
+        "score page",
+        "use arabic script",
+        "english to arabic quiz",
+        "hotkeys",
+        "convert to component",
+        "option to only give right answer",
+        "option to go to next question if wrong answer",
+        "review wrong answers",
+      ],
+    };
+  },
+};
+</script>
