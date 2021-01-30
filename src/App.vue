320449b238a70bcd3095270be11cabb61ffd21f5
Saad Bakht <rotk13@gmail.com>
2021-01-30 00:21:23 -0500

    make temp quiz handler card

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,18 @@
 <template>
   <div class="antialiased">
     <div class="p-16">
-      <quiz-card
-        :heading="heading"
-        :question="question"
-        :choices="choices"
-        :answerId="answer"
-        :index="index"
-        :total="total"
-        @answer="onAnswer"
-        @wrong="onWrong"
-        @right="onRight"
-      ></quiz-card>
+      <quiz-handler></quiz-handler>
     </div>
   </div>
 </template>
 
 <script>
-import QuizCard from "./components/quiz.card.vue";
+import QuizHandler from "./components/quiz.handler.vue";
 
 export default {
   name: "app",
   components: {
-    QuizCard,
-  },
-  data() {
-    return {
-      heading: "Determine the answer",
-      question: "What is your favorite color?",
-      choices: [
-        { id: 1, text: "Green" },
-        { id: 2, text: "Yellow" },
-        { id: 3, text: "Orange" },
-        { id: 4, text: "Blue" },
-      ],
-      answer: 1,
-      index: 1,
-      total: 10,
-      history: [],
-    };
-  },
-  methods: {
-    onAnswer(choice) {
-      this.history.push({
-        question: this.question,
-        choices: this.choices,
-        answered: choice,
-        answer: this.answer,
-      });
-    },
-    onWrong(choice) {
-      console.log("wrong");
-    },
-    onRight(choice) {
-      setTimeout(() => {
-        if (this.index < this.total) {
-          this.index++;
-          this.answer = (this.answer % 4) + 1;
-        }
-      }, 500);
-    },
+    QuizHandler,
   },
 };
 </script>
